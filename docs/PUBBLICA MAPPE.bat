7deaa32340389a84795960aed1dd5e97081b1202
HolovkoLite <sir.holovko@gmail.com>
2026-09-24 15:12:53 +0200

    Aggiornamento automatico mappe - 2026-09-24 15:12:53

--- docs/PUBBLICA MAPPE.bat
+++ docs/PUBBLICA MAPPE.bat
@@ -4,7 +4,7 @@ title Pubblicazione Mappe Nodely
 set "CARTELLA_MAPPE=%~dp0"
 set "CARTELLA_MAPPE=%CARTELLA_MAPPE:~0,-1%"
 
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%USERPROFILE%\Documents\Varie\Mappe Nodely\pubblica-mappe.ps1" -Sorgente "%CARTELLA_MAPPE%"
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%USERPROFILE%\Documents\Varie\Mappe NodeLab\pubblica-mappe.ps1" -Sorgente "%CARTELLA_MAPPE%"
 
 echo.
 echo ------------------------------------------------------------
